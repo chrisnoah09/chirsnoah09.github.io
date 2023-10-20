e0fc87b5603c6b8a194b1e2c5655885bed664bb5
badrobotbr <enderson@gmail.com>
2023-10-20 12:28:40 -0300

    feat: added google lang translation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 $(document).foundation()
+
+$(document).ready(function(){
+  $('#google_translate_element').bind('DOMNodeInserted', function(event) {
+    $('.goog-te-menu-value span:first').html('LANGUAGE');
+    $('.goog-te-menu-frame.skiptranslate').load(function(){
+      setTimeout(function(){
+        $('.goog-te-menu-frame.skiptranslate').contents().find('.goog-te-menu2-item-selected .text').html('LANGUAGE');    
+      }, 100);
+    });
+  });
+});
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'pt', includedLanguages: "en,es,fr,pt", layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
